3777e04d456b475eca2a823dee5a87970eb00176
Mark Kuznetsov <markkuznetsov74@gmail.com>
2024-03-10 20:45:49 +0300

    Created models project, tag, role

--- clanky/settings.py
+++ clanky/settings.py
@@ -108,6 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 STATIC_URL = "static/"
 
 # Default primary key field type
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,63 @@
+from datetime import date
+from email.policy import default
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.urls import reverse
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True, db_index=True)
+
+    def __str__(self) -> str:
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"tag_slug": self.slug})
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("role", kwargs={"role_slug": self.slug})
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    date_started = models.DateField(default=date.today)
+    date_released = models.DateField(default=date.today)
+    repo_link = models.URLField(max_length=300)
+    link = models.URLField(max_length=300, blank=True)
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        db_index=True,
+        validators=[
+            MinLengthValidator(3, message="Минимум 5 символов"),
+            MaxLengthValidator(200, message="Максимум 200 символов"),
+        ],
+    )
+    photo = models.ImageField(
+        upload_to="photos/",
+        default=None,
+        blank=True,
+        null=True,
+        verbose_name="фото",
+    )
+    is_released = models.BooleanField(default=False)
+    role = models.ForeignKey(
+        Role, on_delete=models.PROTECT, related_name="projects", null=True
+    )
+    tags = models.ManyToManyField(Tag, blank=True, related_name="projects")
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("project", kwargs={"project_slug": self.slug})
