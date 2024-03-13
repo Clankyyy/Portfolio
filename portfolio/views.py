7734be2045f2c6d307e9c125aa0191cffe1052e6
Mark Kuznetsov <markkuznetsov74@gmail.com>
2024-03-13 22:15:35 +0300

    Added CBV ProjectList

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,19 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from .models import Project
 from django.http import HttpResponse
+from django.views.generic.list import ListView
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the portfolio index.")
+
+
+class ProjectList(ListView):
+    model = Project
+    template_name = "portfolio/index.html"
+    context_object_name = "projects"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Project.objects.all()
